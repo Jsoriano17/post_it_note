2ce03f288268b06023720cb26497c827059bc160
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-19 09:34:08 -0700

    made some final touches

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,6 +19,18 @@ class NotesController < ApplicationController
       render :new
     end
   end
+  def edit
+    @note = Note.find(params[:id])
+  end
+  def update
+    @note = Note.find(params[:id])
+  
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
   def destroy
     Note.find(params[:id]).destroy
     redirect_to notes_path
